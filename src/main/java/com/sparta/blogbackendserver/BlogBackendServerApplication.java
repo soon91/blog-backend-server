92518a8ba1234659d81054a4e800d36f916f6980
soon91 <vbc1124@gmail.com>
2023-06-27 22:42:17 +0900

    init commit

---
+++ src/main/java/com/sparta/blogbackendserver/BlogBackendServerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.blogbackendserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBackendServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogBackendServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/blogbackendserver/BlogBackendServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.blogbackendserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBackendServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
